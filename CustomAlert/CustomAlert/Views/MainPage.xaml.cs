4887de2da315f80264b0731e2c88e3383ce88ebe
M RIYAZ <rzee.m7@gmail.com>
2017-07-07 22:47:32 +0530

    Add project files.

---
+++ CustomAlert/CustomAlert/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CustomAlert.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
