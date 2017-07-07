92725186000bc3db44c03cffbf2f253bd6c8676e
M RIYAZ <rzee.m7@gmail.com>
2017-07-07 23:11:23 +0530

    Initial Commit

---
+++ CustomAlert/CustomAlert/Controls/AlertView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CustomAlert.Controls
+{
+    public partial class AlertView : ContentView
+    {
+        public AlertView()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+
+        public static readonly BindableProperty TitleProperty =
+                               BindableProperty.Create("Title", typeof(string), typeof(AlertView), null);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty DescriptionProperty =
+                       BindableProperty.Create("Description", typeof(string), typeof(AlertView), null);
+
+        public string Description
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly BindableProperty AlertTypeProperty =
+                       BindableProperty.Create("AlertType", typeof(AlertTypes), typeof(AlertView), AlertTypes.Info,propertyChanged:OnAlertType);
+
+        private static void OnAlertType(BindableObject bindable, object oldValue, object newValue)
+        {
+            var currentView = bindable as AlertView;
+            AlertTypes type = (AlertTypes)newValue;
+            string alertImageName = Constants.GetImage(type);
+
+            if (currentView == null || string.IsNullOrEmpty(alertImageName))
+                return;
+
+            currentView.AlertImage = ImageSource.FromResource(alertImageName);
+        }
+
+        public AlertTypes AlertType
+        {
+            get { return (AlertTypes)GetValue(AlertTypeProperty); }
+            set { SetValue(AlertTypeProperty, value); }
+        }
+
+        private ImageSource _alertImage;
+
+        public ImageSource AlertImage
+        {
+            get { return _alertImage; }
+            set { _alertImage = value; }
+        }
+
+
+        #endregion
+    }
+}
---
+++ CustomAlert/CustomAlert/Helper/Constants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAlert
+{
+    public class Constants
+    {
+        public const string SuccessImage = "success";
+        public const string FailImage = "fail";
+        public const string InfoImage = "info";
+
+        public static string GetImage(AlertTypes type)
+        {
+            string fileName = string.Empty;
+            switch (type)
+            {
+                case AlertTypes.Success:
+                    fileName = SuccessImage; ;
+                    break;
+                case AlertTypes.Error:
+                    fileName = FailImage;
+                    break;
+                case AlertTypes.Info:
+                    fileName = InfoImage;
+                    break;
+                default:
+                    break;
+            }
+            return string.Format("{0}.{1}.png", typeof(Constants).GetTypeInfo().Assembly.GetName(), fileName);
+        }
+    }
+    public enum AlertTypes
+    {
+        Success,
+        Error,
+        Info
+    }
+}
